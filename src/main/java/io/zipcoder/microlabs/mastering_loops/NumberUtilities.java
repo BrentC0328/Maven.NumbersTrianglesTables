3b583fca0ee42a6afdb727dba8ff312f31e2a97e
Brent <BrentC0328@gmail.com>
2023-06-30 00:10:20 -0400

    Number Utilities completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,35 +2,82 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
+
+    public static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String numbers = "";
+
+        for(int i = start; i <+ stop; i++){
+            Integer tempNumber = i;
+            if(isEven(i)){
+                numbers = numbers + tempNumber;
+            }
+        }
+
+        return numbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String numbers = "";
+
+        for(int i = start; i < stop; i++){
+            Integer tempNumber = i;
+            if(!isEven(i)){
+                numbers += tempNumber;
+            }
+        }
+
+        return numbers;
+
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squaredNumbers = "";
+
+        for (int i = start; i < stop; i += step){
+            squaredNumbers += (i * i);
+
+        }
+
+
+        return squaredNumbers;
     }
 
     public static String getRange(int start) {
-        return null;
+        return getRange(0, start, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i += step){
+            range += i;
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String exponents = "";
+
+        for (int i = start; i < stop; i += step){
+            int exponentHolder = 1;
+
+            for(int j = 1; j <= exponent; j++){
+                exponentHolder *= i;
+            }
+            exponents += exponentHolder;
+        }
+
+        return exponents;
     }
 }
